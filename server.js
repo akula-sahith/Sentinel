074de7813220c54d755469adcd7d8236999c6440
Sahith Akula <akulasahith268@gmail.com>
2026-04-13 21:02:02 +0530

    Added

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.get('/', (req, res) => {
   res.send('Hello from Node.js App Engine!');
 });
 
-app.listen(5000, "0.0.0.0", () => {
-  console.log("Server running");
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
